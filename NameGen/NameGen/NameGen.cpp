824dab2c2869f88e1fdc809a13b1ff757c4e0b23
David S <b1053474@live.tees.ac.uk>
2023-08-04 22:45:02 +0100

    Still Unfinished

--- NameGen/NameGen/NameGen.cpp
+++ NameGen/NameGen/NameGen.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
-
+#include <random>
 
 using namespace std;
 const int k_NumberOfNames = 100;
@@ -11,69 +11,88 @@ const int k_NumberOfNames = 100;
 void readFile(vector<string>vector,const string &fileName);
 string generateNames(vector<string>fForename, vector<string>mForename, vector<string> Surname);
 
+std::vector<string>mFirstName;
+std::vector<string>fFirstName;
+std::vector<string>fLastName;
 
 int main()
 {
+    std::random_device rand;
+    std::uniform_int_distribution<int>dist(1, 50);
+
     int numberRand;
     int numberOfElements=100;
-    vector<string>mFirstName = {};
-    vector<string>fFirstName = {};
-    vector<string>fLastName = {};
     
-    vector<string>namesGenerated = {};
-
 
     readFile(fFirstName, "femaleNames");
     readFile(mFirstName,"maleNames");
     readFile(fLastName, "lastNames");
+    
 
-    for (size_t i = 0; i < numberOfElements; ++i)
-    {
-        string tempVar=generateNames(fFirstName,mFirstName,fLastName);
-        namesGenerated.push_back(tempVar);
+    /*for (string p : mFirstName) {
+        std::cout << p << "\n";
+    }*/
+    for (int i = 0; i < 15; ++i) {
+        
+        if (rand()%2+1==2) {
+            std::cout << i << ". " << mFirstName.at(dist(rand)) << " " << fLastName.at(dist(rand))<<"\n";
+        }
+        else {
+            std::cout << i << ". " << fFirstName.at(dist(rand)) << " " << fLastName.at(dist(rand)) << "\n";
+        }
+        
+        
     }
-    
-    
-
-    
+    for (auto p: mFirstName) {
+        p.erase();
+    }
+    for (auto p : fFirstName) {
+         p.erase();
+    }
+    for (auto p : fLastName) {
+        p.erase();
+    }
     return 0;
 }
 
 void readFile(vector<string> vector, const string &fileName) {
     string line;
-    fstream file; //object of fstream class 
+    ifstream file; //object of fstream class 
 
-    file.open("List\\French\\" + fileName + ".txt", ios::in);
+    file.open("List\\French\\" + fileName + ".txt");
 
-    if (!file)
+    if (file.fail())
     {
         cout << "Error in opening file!!!" << endl;
         return;
     }
     // Pushes the Contents into the Vector
-    while (getline(file, line)) {
+    while (!file.eof()) {
+        getline(file,line);
         vector.push_back(line);
     }
-
+    for (auto l : vector) {
+        std::cout << l<<"\n";
+    }
     file.close(); //close file
 }
 
 string generateNames(vector<string>fForename, vector<string>mForename, vector<string> Surname){
-    string name;
+    string name="";
     int rnumber = rand() % k_NumberOfNames;
-    int chooseGender = rand() % 2 + 1;
+    
 
-    if (chooseGender == 1)
+    if (rand() % 2 + 1 == 1)
     {
-       
-        name = fForename[rand()%99] + " " + Surname[rand()%99];
-        std::cout << name;
+        fForename[rand() % 99 + 1] + " " + Surname[rand() % 99 + 1];
+        //name.append( );
+        //std::cout << name;
         return name;
     }
     else 
     {
-        name = fForename[rand() % 99] + " " + Surname[rand() % 99];
-        std::cout << name;
+        name.append(mForename[rand()%49+1] + " " + Surname[rand() % 99+1]);
+        //std::cout << name;
         return name;
     }
 
