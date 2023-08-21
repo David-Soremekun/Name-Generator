2d45da10f0afa8c89fd0063aaad30a1ea196fd75
David S <b1053474@live.tees.ac.uk>
2023-08-21 14:52:41 +0100

    Added Japanese Naming Conventions

--- GeneratorName/Name.cpp
+++ GeneratorName/Name.cpp
@@ -8,47 +8,65 @@
 Name::Name()
 {
     srand(time(NULL));
+   
     
-    const std::string& frenchLastName = "List\\French\\lastNames.txt";
-    const std::string& frFemaleName = "List\\French\\femaleNames.txt";
-    const std::string& frMaleName = "List\\French\\maleNames.txt";
+}
 
-    const std::string& spLastName = "List\\Spanish\\lastNames.txt";
-    const std::string& spFemaleName = "List\\Spanish\\femaleNames.txt";
-    const std::string& spMaleName = "List\\Spanish\\maleNames.txt";
+Name::~Name()
+{
+    delete[] swFirstNameB;
+    delete[] swFirstNameG;
+    delete[] swFamilyName;
     
-    const std::string& rsLastName = "List\\Russian\\lastNames.txt";
-    const std::string& rsFemaleName = "List\\Russian\\femaleNames.txt";
-    const std::string& rsMaleName = "List\\Russian\\maleNames.txt";
+    delete[] jpFirstNameB;
+    delete[] jpFirstNameG;
+    delete[] jpFamilyName;
 
-    const std::string&itLastName = "List\\Italian\\lastNames.txt";
-    const std::string& itFemaleName = "List\\Italian\\femaleNames.txt";
-    const std::string& itMaleName = "List\\Italian\\maleNames.txt";
+    delete[] frFirstNameB;
+    delete[] frFirstNameG;
+    delete[] frFamilyName;
 
-    const std::string& swLastName = "List\\Swedish\\lastNames.txt";
-    const std::string& swFemaleName = "List\\Swedish\\femaleNames.txt";
-    const std::string& swMaleName = "List\\Swedish\\maleNames.txt";
+    delete[] spFirstNameB;
+    delete[] spFirstNameG;
+    delete[] spFamilyName;
 
-    readFile(frMaleName, frFirstNameB, 50);
-    readFile(frFemaleName, frFirstNameG, 100);
-    readFile(frenchLastName, frFamilyName, 100);
+    delete[] itFirstNameB;
+    delete[] itFirstNameG;
+    delete[] itFamilyName;
 
-    readFile(spMaleName, spFirstNameB, 100);
-    readFile(spFemaleName, spFirstNameG, 100);
-    readFile(spLastName, spFamilyName, 196);
+    delete[] rsFirstNameB;
+    delete[] rsFirstNameG;
+    delete[] rsFamilyName;
 
-    readFile(rsMaleName, rsFirstNameB, 63);
-    readFile(rsFemaleName, rsFirstNameG, 123);
-    readFile(rsLastName, rsFamilyName, 99);
+}
 
-    readFile(itMaleName, itFirstNameB, 383);
-    readFile(itFemaleName, itFirstNameG, 198);
-    readFile(itLastName, itFamilyName, 791);
+void Name::readFile(const std::string &filename, std::string *testArray, const int SIZE)
+{
+    std::string inFileName = filename;
+    std::ifstream inFile;
+    inFile.open(inFileName.c_str());
 
-    readFile(swMaleName, swFirstNameB, 198);
-    readFile(swFemaleName, swFirstNameG, 178);
-    readFile(swLastName, swFamilyName, 710);
+    if (inFile.is_open())
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            inFile >> testArray[i];
+            //std::cout << testArray[i] << "\n"; //Test To Ouput Values
+        }
 
+        inFile.close(); // CLose input file
+    }
+    //free(testArray);
+}
+
+void Name::printNames()
+{
+    for (int i = 0; i < listName.size(); ++i)
+        std::cout << listName[i] << std::endl;
+
+    listName.clear();
+    deleteNames();
+    
 }
 
 void Name::deleteNames()
@@ -57,10 +75,19 @@ void Name::deleteNames()
         listName[i].erase();
     }
 }
+
 void Name::createSpanishNames() 
 {
-    int genderChoice;
+    const std::string& spLastName = "List\\Spanish\\lastNames.txt";
+    const std::string& spFemaleName = "List\\Spanish\\femaleNames.txt";
+    const std::string& spMaleName = "List\\Spanish\\maleNames.txt";
+    
+    readFile(spMaleName, spFirstNameB, 100);
+    readFile(spFemaleName, spFirstNameG, 100);
+    readFile(spLastName, spFamilyName, 196);
 
+
+    int genderChoice;
     int amountRandfirstName;
     int amountRandSecondName;
     for (int i = 0; i < nameCount; i++)
@@ -83,36 +110,18 @@ void Name::createSpanishNames()
     printNames();
 }
 
-void Name::readFile(const std::string &filename, std::string *testArray, const int SIZE)
-{
-    std::string inFileName = filename;
-    std::ifstream inFile;
-    inFile.open(inFileName.c_str());
-
-    if (inFile.is_open())
-    {
-        for (int i = 0; i < SIZE; i++)
-        {
-            inFile >> testArray[i];
-            //std::cout << testArray[i] << "\n"; //Test To Ouput Values
-        }
-
-        inFile.close(); // CLose input file
-    }
-}
-
-void Name::printNames()
-{
-    for (int i = 0; i < listName.size(); ++i)
-        std::cout << listName[i] << std::endl;
-
-    listName.clear();
-    deleteNames();
-    
-}
-
 void Name::createFrenchNames() 
 {
+    const std::string& frenchLastName = "List\\French\\lastNames.txt";
+    const std::string& frFemaleName = "List\\French\\femaleNames.txt";
+    const std::string& frMaleName = "List\\French\\maleNames.txt";
+
+
+    readFile(frMaleName, frFirstNameB, 50);
+    readFile(frFemaleName, frFirstNameG, 100);
+    readFile(frenchLastName, frFamilyName, 100);
+
+
     srand(time(NULL));
     int maleFirstNameGen;
     int genFirstName;
@@ -145,6 +154,15 @@ void Name::createFrenchNames()
 
 void Name::createRussianNames()
 {
+    const std::string& rsLastName = "List\\Russian\\lastNames.txt";
+    const std::string& rsFemaleName = "List\\Russian\\femaleNames.txt";
+    const std::string& rsMaleName = "List\\Russian\\maleNames.txt";
+     
+    readFile(rsMaleName, rsFirstNameB, 63);
+    readFile(rsFemaleName, rsFirstNameG, 123);
+    readFile(rsLastName, rsFamilyName, 99);
+
+
     srand(time(NULL));
     int maleFirstNameGen;
     int genFirstName;
@@ -180,11 +198,19 @@ void Name::createRussianNames()
 void Name::createItalianNames()
 {
     srand(time(NULL));
+
+    const std::string& itLastName = "List\\Italian\\lastNames.txt";
+    const std::string& itFemaleName = "List\\Italian\\femaleNames.txt";
+    const std::string& itMaleName = "List\\Italian\\maleNames.txt";
+
+    readFile(itMaleName, itFirstNameB, 383);
+    readFile(itFemaleName, itFirstNameG, 198);
+    readFile(itLastName, itFamilyName, 791);
+
     int maleFirstNameGen;
     int genFirstName;
     int genSecondName;
     int genderChoice;
-
 
     for (int i = 0; i < nameCount; i++) {
         genderChoice = rand() % 2 + 1;
@@ -212,6 +238,15 @@ void Name::createItalianNames()
 void Name::createSwedishNames()
 {
     srand(time(NULL));
+
+    const std::string& swLastName = "List\\Swedish\\lastNames.txt";
+    const std::string& swFemaleName = "List\\Swedish\\femaleNames.txt";
+    const std::string& swMaleName = "List\\Swedish\\maleNames.txt";
+
+    readFile(swMaleName, swFirstNameB, 198);
+    readFile(swFemaleName, swFirstNameG, 178);
+    readFile(swLastName, swFamilyName, 710);
+
     int maleFirstNameGen;
     int genFirstName;
     int genSecondName;
@@ -239,4 +274,44 @@ void Name::createSwedishNames()
         }
     }
     printNames();
+}
+
+void Name::createJapaneseNames()
+{
+    srand(time(NULL));
+    int maleFirstNameGen;
+    int genFirstName;
+    int genSecondName;
+    int genderChoice;
+
+    const std::string& jpLastName = "List\\Japanese\\lastNames.txt";
+    const std::string& jpFemaleName = "List\\Japanese\\femaleNames.txt";
+    const std::string& jpMaleName = "List\\Japanese\\maleNames.txt";
+
+    readFile(jpMaleName, jpFirstNameB, 1398);
+    readFile(jpFemaleName, jpFirstNameG, 499);
+    readFile(jpLastName, jpFamilyName, 110);
+
+    for (int i = 0; i < nameCount; i++) {
+        genderChoice = rand() % 2 + 1;
+
+        maleFirstNameGen = rand() % 1398;
+        genFirstName = rand() % 499;
+        genSecondName = rand() % 110;
+
+        switch (genderChoice) {
+        case 1:
+            name = jpFamilyName[genSecondName] + " " + jpFirstNameB[maleFirstNameGen] + " [M]";
+            listName.push_back(name);
+            break;
+        case 2:
+            name = jpFamilyName[genSecondName] + " " + jpFirstNameG[genFirstName] + " [F]";
+            listName.push_back(name);
+            break;
+        default:
+            break;
+        }
+    }
+    printNames();
+
 }
